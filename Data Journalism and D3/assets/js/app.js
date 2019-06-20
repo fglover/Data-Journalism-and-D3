2bbe19c1cabdae173fe218a67b73f85cafc031b8
fglover <fgloverucfbc@gmail.com>
2019-06-19 21:22:59 -0400

    Adding files

---
+++ Data Journalism and D3/assets/js/app.js
@@ -0,0 +1,107 @@
+var svgWidth = 960
+var svgHeight = 660;
+
+var chartMargin = {
+    top: 30, 
+    right: 30, 
+    bottom: 30, 
+    left: 30
+};
+
+//Chart Area Dimensions
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+
+//Making Chart
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("class", "chart");
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+
+d3.csv("assets/data/data.csv").then (function(infoData) {
+    infoData.forEach(function(data) {
+        visualize(data);
+        data.InfoData = +data.InfoData;
+        data.poverty = +data.poverty
+    });
+ 
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(infoData, d => d.poverty) - 1.25, d3.max(infoData, d => d.poverty)])
+        .range([0, chartWidth]);
+    
+    var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(infoData, d => d.healthcare) - 2, d3.max(infoData, d => d.healthcare) + 1])
+        .range([chartHeight, 0]);
+    
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(infoData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "10") 
+        .attr("fill", "lightorange")
+        .attr("opacity", ".5"); 
+
+
+
+    var textGroup = chartGroup.selectAll("p")
+        .data(infoData)
+        .enter()
+        .append("p")
+        .attr("left", d => xLinearScale(d.poverty))
+        .attr("top", d => yLinearScale(d.healthcare))
+        .attr("p", d => d.abbr)
+
+
+
+    var toolTip = d3
+        .tip()
+        .attr("class", "d3-tip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.state}<br>poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`)
+        });
+    
+    chartGroup.call(toolTip);
+
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+    })
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)") //vertical text
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("y", 0 - chartMargin.left + 30)
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Healthcare");
+
+    
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 20})`)
+        .attr("class", "axisText")
+        .text("Poverty");
+});
